189871fa5b6def0b335f3ed1f81c0990c08532c1
Yang Ming <yangming0116@163.com>
2019-07-31 13:15:10 +0800

    修船后更新船只信息

---
+++ src/JrApi/Server/Boat/InstantRepairShips.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\JrApi\Server\Boat;
+
+use App\Http;
+use App\JrApi\BaseJrApi;
+use App\Model\PlayerInfo;
+
+class InstantRepairShips extends BaseJrApi {
+
+    public function __construct($request) {
+        parent::__construct($request);
+    }
+
+    public function after($response) {
+
+        parent::after($response);
+
+        if ($this->uid === null) {
+            return;
+        }
+
+        $body = $this->response->get_body();
+
+        $str = zlib_decode($body);
+        $json = json_decode($str, true);
+
+        if ($json === null) {
+            return;
+        }
+
+        if (!isset($json['shipVOs'])) {
+            return;
+        }
+
+        $player_info = new PlayerInfo($this->uid);
+        $player_info->set_ships($json['shipVOs'])->save();
+    }
+
+}
